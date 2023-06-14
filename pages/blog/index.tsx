22d69c762d15fb83ad95fa9080066411032120c8
PenguinCabinet <kage2.kage22@gmail.com>
2023-06-14 23:18:43 +0900

    Multi-year support

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Layout } from "../../components/Layout";
 import { useRouter } from "next/router";
 
+/*
 const redurectUrl = "/blog/2020";
 
 const RedirectPage: NextPage = () => {
@@ -33,3 +34,27 @@ const Loading = () => {
     </Layout>
   );
 };
+*/
+
+const redurectUrl_template = "/blog/";
+
+const Blog_list: NextPage = () => {
+  const years = ["2020", "2022"];
+  return (
+    <Layout
+      title="SecHack365 参加記 まとめ"
+      description="SecHack365に参加したトレーニーによる参加ブログのまとめです。"
+    >
+      <h1>年度 SecHack365 参加記 まとめ【随時更新】</h1>
+      {years.map((year, index) => (
+        <li key={index}>
+          <Link href={redurectUrl_template + year}>
+            <a>{year}年度 参加記 まとめ</a>
+          </Link>
+        </li>
+      ))}
+    </Layout>
+  );
+};
+
+export default Blog_list;
